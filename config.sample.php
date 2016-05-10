deb8e1c6d0fd90a6ae1aa4779bf96a10189f8cad
Johannes Siipola <johannes@exove.fi>
2016-05-10 17:39:42 +0300

    Initial commit

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+
+const TWITTER_API_KEY = 'TWITTER_API_KEY';
+const TWITTER_API_SECRET = 'TWITTER_API_SECRET';
+const TWITTER_SEARCH_USERNAME = '@exove';
+const CACHE_ENABLED = false;
+const CACHE_LIFETIME = 15;
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require 'vendor/autoload.php';
+require 'config.php';
+
+$container = new \Slim\Container;
+
+$cache = new Illuminate\Cache\FileStore(new Illuminate\Filesystem\Filesystem(), 'cache');
+
+$container['cache'] = $cache;
+
+$app = new Slim\App($container);
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+$app->get('/api', function(Request $request, Response $response, $arguments) {
+
+    $cachedTweets = $this->cache->get('tweets');
+
+    if ($cachedTweets) {
+        return $response->withJson($cachedTweets, 200, JSON_PRETTY_PRINT);
+    }
+
+    $client = new Freebird\Services\freebird\Client();
+    $client->init_bearer_token(TWITTER_API_KEY, TWITTER_API_SECRET);
+    $tweets = $client->api_request('statuses/user_timeline.json', ['screen_name' => TWITTER_SEARCH_USERNAME]);
+    $tweets = json_decode($tweets);
+
+    if (CACHE_ENABLED) {
+        $this->cache->put('tweets', $tweets, CACHE_LIFETIME);
+    }
+
+    return $response->withJson($tweets, 200, JSON_PRETTY_PRINT);
+});
+
+$app->get('/', function(Request $request, Response $response, $arguments) {
+
+});
+
+$app->run();
