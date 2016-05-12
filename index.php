e1e776c0bd23a7754872549ed4a4995a7cb382d5
Johannes Siipola <johannes@exove.fi>
2016-05-12 10:42:34 +0300

    Fix retweets

--- index.php
+++ index.php
@@ -42,7 +42,17 @@ $app->get('/api', function(Request $request, Response $response, $arguments) {
     $client = new Freebird\Services\freebird\Client();
     $client->init_bearer_token(TWITTER_API_KEY, TWITTER_API_SECRET);
     $tweets = $client->api_request('statuses/user_timeline.json', ['screen_name' => TWITTER_SEARCH_USERNAME]);
-    $tweets = json_decode($tweets);
+    $tweets = collect(json_decode($tweets));
+    $tweets = $tweets->map(function($tweet){
+        if (property_exists($tweet, 'retweeted_status')) {
+            $userName = $tweet->user->name;
+            $tweet->text = $tweet->retweeted_status->text;
+            $tweet->user = $tweet->retweeted_status->user;
+            $tweet->user->name = $userName . ' ' . html_entity_decode('&#x1f504;', 0, 'UTF-8') . ' ' . $tweet->user->name;
+        }
+        $tweet->text = html_entity_decode($tweet->text);
+        return $tweet;
+    });
 
     if (CACHE_ENABLED) {
         $this->cache->put('tweets', $tweets, CACHE_LIFETIME);
